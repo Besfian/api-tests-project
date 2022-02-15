33b4091394c62f4192c8093023e84234051689fe
besfian <besfian@mail.ru>
2022-02-16 02:08:03 +0300

    *

---
+++ src/test/java/ru/mail/besfian/config/CredentialsConfig.java
@@ -0,0 +1,18 @@
+package ru.mail.besfian.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "file:/tmp/credentials.properties",
+        "classpath:config/credentials.properties"
+})
+public interface CredentialsConfig extends Config {
+
+    @Key("userName")
+    String userName();
+
+    @Key("password")
+    String password();
+}
